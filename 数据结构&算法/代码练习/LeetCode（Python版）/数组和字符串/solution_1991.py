c092ed1328d5a455098d1f01c72e0288a8ce82d1
申杉杉 <467638484@qq.com>
2023-08-23 00:26:56 +0800

    刷题
    刷题

---
+++ 数据结构&算法/代码练习/LeetCode（Python版）/数组和字符串/solution_1991.py
@@ -0,0 +1,21 @@
+from typing import List
+
+"""
+1991. 找到数组的中间位置
+"""
+
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        sums = sum(nums)
+        left = 0
+
+        for i in range(len(nums)):
+            if left * 2 + nums[i] == sums:
+                return i
+            left += nums[i]
+
+        return -1
---
+++ 数据结构&算法/代码练习/LeetCode（Python版）/数组和字符串/solution_35.py
@@ -0,0 +1,27 @@
+from typing import List
+
+"""
+35. 搜索插入位置
+"""
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if min(nums) >= target:
+            return 0
+        if max(nums) < target:
+            return len(nums)
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+
+        return left
